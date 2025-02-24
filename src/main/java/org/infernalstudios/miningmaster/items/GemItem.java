c78bd65a36714fe22892d4de39488f928be09e8e
Kārlis Čerņavskis <karliscern@gmail.com>
2025-02-24 21:39:13 +0200

    Changed missed Component.literal to Component.translatable, including in gem tooltips, made gem tooltips customizable

--- src/main/java/org/infernalstudios/miningmaster/items/GemItem.java
+++ src/main/java/org/infernalstudios/miningmaster/items/GemItem.java
@@ -1,6 +1,7 @@
 package org.infernalstudios.miningmaster.items;
 
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -15,14 +16,16 @@ public class GemItem extends Item {
         super(new Item.Properties());
     }
 
-
-
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
         if (Screen.hasShiftDown()) {
-            tooltip.add(Component.literal("\u00A7dCombine with an item in a smithing table to enchant!"));
+            tooltip.add(
+                Language.getInstance().has(this.getDescriptionId() + ".tooltip") ?
+                    Component.translatable(this.getDescriptionId() + ".tooltip") :
+                    Component.translatable("miningmaster.item.tooltip.fallback_gem")
+            );
         } else {
-            tooltip.add(Component.literal("\u00A78Hold Shift for Instructions"));
+            tooltip.add(Component.translatable("miningmaster.item.tooltip.hold_shift"));
         }
     }
 }
